5ca0a32467a83c6f0c512632256164eb6ceead56
Raphael B <drcraftington@gmail.com>
2018-07-19 16:27:20 +0200

    przeniosłem funkcje drawing() z init_drawing do klasy game w Levi Program

--- Levi Program.py
+++ Levi Program.py
@@ -10,14 +10,16 @@ import pygame, sys
 
 #I will import my modules
 import init_dimensions
-import init_drawing
+# import init_drawing
 from Settings import *
+
 
 
 class Game:  
     def __init__(self):
         # initialize game window etc
         pygame.init()
+        self.screen = pygame.display.set_mode((WIDTHSCREEN, HEIGHTSCREEN))
         pygame.display.set_caption("Neuros")
         self.clock = pygame.time.Clock()
         
@@ -25,16 +27,16 @@ class Game:
     
     def new(self):
         # start a new game
-        init_drawing.drawing()
-    
+        pass
+        
     def run(self):
         # Gameloop      
         self.playing = True
-            while self.playing:
-                self.clock.tick(FPS)
-                self.events()
-                self.update()
-                self.draw()
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
                   
     def update(self):
         # Gameloop - Update
@@ -43,14 +45,28 @@ class Game:
     def events(self):
         # Gameloop - Events
         for event in pygame.event.get():
-        #Exit
-        if event.type == pygame.QUIT:
-            if self.playing:
-                self.playing = False
-            self.running = False
+        
+            #Exit
+            if event.type == pygame.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
     
     def draw(self):
         # Gameloop - Draw
+        self.screen.fill(background_color)
+        
+        for i in range(0,6):
+            for ii in range(0,10):
+                texture = pygame.Rect(left_x_list[i][ii], up_y_list[i], widthcard, heightcard)
+                pygame.draw.rect(self.screen, PLACE_FOR_CARD_COLOR[i], texture)
+
+        for i in range(0,2):
+            texture = pygame.Rect(up_deck_list_left_x[i], up_deck_up_y, widthcard, heightcard)
+            pygame.draw.rect(self.screen, DECK_LINE_COLOR, texture)
+            texture = pygame.Rect(down_deck_list_left_x[i], down_deck_up_y, widthcard, heightcard)
+            pygame.draw.rect(self.screen, DECK_LINE_COLOR, texture)
+        
         pygame.display.flip()
     
     def show_start_screen(self):
