2e546378ac80bb39d0452c3c8fb44b936e61f17c
DrCrafty <34989904+DrCrafty@users.noreply.github.com>
2018-07-19 12:10:13 +0200

    Update Levi Program.py
    trochę pozmieniałem

--- Levi Program.py
+++ Levi Program.py
@@ -7,19 +7,75 @@
 #So i will import Pygame and sys
 
 import pygame, sys
-pygame.init()
 
 #I will import my modules
 import init_dimensions
 import init_drawing
+from Settings import *
 
-#Drawing screen
-init_drawing.drawing()
 
-#Game Loop
-while True:
-    #Handling event
-    for event in pygame.event.get():
+class Game:  
+    def __init__(self):
+        # initialize game window etc
+        pygame.init()
+        pygame.display.set_caption("Neuros")
+        self.clock = pygame.time.Clock()
+        
+        self.running = True
+    
+    def new(self):
+        # start a new game
+        init_drawing.drawing()
+    
+    def run(self):
+        # Gameloop      
+        self.playing = True
+            while self.playing:
+                self.clock.tick(FPS)
+                self.events()
+                self.update()
+                self.draw()
+                  
+    def update(self):
+        # Gameloop - Update
+        pass
+    
+    def events(self):
+        # Gameloop - Events
+        for event in pygame.event.get():
         #Exit
         if event.type == pygame.QUIT:
-            sys.exit(0)
+            if self.playing:
+                self.playing = False
+            self.running = False
+    
+    def draw(self):
+        # Gameloop - Draw
+        pygame.display.flip()
+    
+    def show_start_screen(self):
+        # Loads the start screen
+        pass
+    
+    def game_over_screen(self):
+        # Loads the game over screen
+        pass
+    
+    def connect_to_server(self):
+        # Connect to server
+        pass
+    
+    
+g = Game()
+g.show_start_screen()
+while g.running:
+    g.new()
+    g.game_over_screen()
+    
+pygame.quit()
+
+            
+         
+    
+    
+    
