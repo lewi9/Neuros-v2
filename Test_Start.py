517f1e3f491635f6acf31ba9137e994c882c6a27
Raphael B <drcraftington@gmail.com>
2018-07-28 10:38:02 +0200

    dadałem testowy plik do odpalania serwer'a i dwa klienty

---
+++ Test_Start.py
@@ -0,0 +1,5 @@
+import subprocess 
+
+process = subprocess.Popen(["python3 Start\\ Server.py"], shell = True)
+process = subprocess.Popen(["python3 Index.py"], shell = True)
+process = subprocess.Popen(["python3 Index.py"], shell = True)
